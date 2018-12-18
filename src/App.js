9ba2f9368a2e8fa46e72e9c2bd6327f3c66664c9
Maris Herrera <maris.lizete.herrera@gmail.com>
2018-12-18 09:35:31 -0700

    finish skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import BlogForm from './BlogForm';
 import BlogPost  from './BlogPost';
+// import BlogShow from './BlogShow'
 import './App.css';
 
 const App = () => (
   <div >
     <BlogForm />
     <BlogPost />
+    {/* <BlogShow /> */}
   </div>
 )
 
